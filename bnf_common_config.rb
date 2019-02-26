9616d6ba747667805119233b579809f488e2bb82
Justin Coyne <jcoyne@justincoyne.com>
2019-02-26 11:10:05 -0600

    Use TrajectPlus for the functions that have been extracted there

--- bnf_common_config.rb
+++ bnf_common_config.rb
@@ -5,6 +5,7 @@ require 'dlme_json_resource_writer'
 require 'macros/dlme'
 
 extend Macros::DLME
+extend TrajectPlus::Macros
 extend TrajectPlus::Macros::Xml
 
 settings do
@@ -15,18 +16,18 @@ end
 record = 'srw:record/srw:recordData/oai_dc:dc'
 
 # Cho Required
-to_field 'id', extract_xml("#{record}/dc:identifier", LOC_NS), strip
-to_field 'cho_title', extract_xml("#{record}/dc:title", LOC_NS), strip
+to_field 'id', extract: extract_xml("#{record}/dc:identifier", LOC_NS), transform: transform(strip: true)
+to_field 'cho_title', extract: extract_xml("#{record}/dc:title", LOC_NS), transform: transform(strip: true)
 
 # Cho Other
-to_field 'cho_date', extract_xml("#{record}/dc:date", LOC_NS), strip
-to_field 'cho_description', extract_xml("#{record}/dc:description", LOC_NS), strip
-to_field 'cho_dc_rights', extract_xml("#{record}/dc:rights", LOC_NS), strip
-to_field 'cho_format', extract_xml("#{record}/dc:format", LOC_NS), strip
-to_field 'cho_publisher', extract_xml("#{record}/dc:publisher", LOC_NS), strip
-to_field 'cho_relation', extract_xml("#{record}/dc:relation", LOC_NS), strip
-to_field 'cho_source', extract_xml("#{record}/dc:source", LOC_NS), strip
-to_field 'cho_subject', extract_xml("#{record}/dc:subject", LOC_NS), strip
+to_field 'cho_date', extract: extract_xml("#{record}/dc:date", LOC_NS), transform: transform(strip: true)
+to_field 'cho_description', extract: extract_xml("#{record}/dc:description", LOC_NS), transform: transform(strip: true)
+to_field 'cho_dc_rights', extract: extract_xml("#{record}/dc:rights", LOC_NS), transform: transform(strip: true)
+to_field 'cho_format', extract: extract_xml("#{record}/dc:format", LOC_NS), transform: transform(strip: true)
+to_field 'cho_publisher', extract: extract_xml("#{record}/dc:publisher", LOC_NS), transform: transform(strip: true)
+to_field 'cho_relation', extract: extract_xml("#{record}/dc:relation", LOC_NS), transform: transform(strip: true)
+to_field 'cho_source', extract: extract_xml("#{record}/dc:source", LOC_NS), transform: transform(strip: true)
+to_field 'cho_subject', extract: extract_xml("#{record}/dc:subject", LOC_NS), transform: transform(strip: true)
 
 # Agg
 to_field 'agg_provider', provider
@@ -34,13 +35,13 @@ to_field 'agg_data_provider', data_provider
 to_field 'agg_is_shown_at' do |_record, accumulator, context|
   accumulator << transform_values(
     context,
-    'wr_id' => [extract_xml('srw:record/srw:extraRecordData/link', LOC_NS, [:trim])]
+    'wr_id' => [extract_xml('srw:record/srw:extraRecordData/link', LOC_NS, [:strip])]
   )
 end
 
 to_field 'agg_preview' do |_record, accumulator, context|
   accumulator << transform_values(
     context,
-    'wr_id' => [extract_xml('srw:record/srw:extraRecordData/thumbnail', LOC_NS, [:trim])]
+    'wr_id' => [extract_xml('srw:record/srw:extraRecordData/thumbnail', LOC_NS, [:strip])]
   )
 end
--- bnf_salt_config.rb
+++ bnf_salt_config.rb
@@ -2,7 +2,11 @@
 
 record = 'srw:record/srw:recordData/oai_dc:dc'
 
-to_field 'cho_contributor', extract_xml("#{record}/dc:contributor", LOC_NS), strip
-to_field 'cho_creator', extract_xml("#{record}/dc:creator", LOC_NS), strip
-to_field 'cho_edm_type', extract_xml("#{record}/dc:type", LOC_NS), prepend('text |'), split('|'), first_only, strip, default('text')
-to_field 'cho_language', extract_xml("#{record}/dc:language", LOC_NS), strip, gsub(/Ottoman Turkish/, 'Turkish, Ottoman')
+to_field 'cho_contributor', extract: extract_xml("#{record}/dc:contributor", LOC_NS),
+                            transform: transform(strip: true)
+to_field 'cho_creator', extract: extract_xml("#{record}/dc:creator", LOC_NS),
+                        transform: transform(strip: true)
+to_field 'cho_edm_type',  extract: extract_xml("#{record}/dc:type", LOC_NS),
+                          transform: transform(strip: true, prepend: 'text |', split: '|', default: 'text')
+to_field 'cho_language',  extract: extract_xml("#{record}/dc:language", LOC_NS),
+                          transform: transform(strip: true, gsub: [/Ottoman Turkish/, 'Turkish, Ottoman'])
