17b4bcf2a9ff3099922d570b182cff6b29e4531b
Justin Coyne <jcoyne@justincoyne.com>
2019-02-28 14:46:34 -0600

    Use the new SRW macro

--- bnf_ifea_config.rb
+++ bnf_ifea_config.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
-record = 'srw:record/srw:recordData/oai_dc:dc'
-
-to_field 'cho_contributor', extract_xml("#{record}/dc:contributor", Macros::BNF::NS), strip
-to_field 'cho_creator', extract_xml("#{record}/dc:creator", Macros::BNF::NS), split('.'), first_only, strip
-to_field 'cho_creator', extract_xml("#{record}/dc:creator[2]", Macros::BNF::NS), split('.'), first_only, strip
-to_field 'cho_edm_type', extract_xml("#{record}/dc:type", Macros::BNF::NS), first_only, strip, translation_map('types'), default('image')
-to_field 'cho_language', extract_xml("#{record}/dc:language", Macros::BNF::NS), strip, translation_map('marc_languages')
+to_field 'cho_contributor', extract_srw('dc:contributor'), strip
+to_field 'cho_creator', extract_srw('dc:creator'), split('.'), first_only, strip
+to_field 'cho_creator', extract_srw('dc:creator[2]'), split('.'), first_only, strip
+to_field 'cho_edm_type', extract_srw('dc:type'), first_only, strip, translation_map('types'), default('image')
+to_field 'cho_language', extract_srw('dc:language'), strip, translation_map('marc_languages')
