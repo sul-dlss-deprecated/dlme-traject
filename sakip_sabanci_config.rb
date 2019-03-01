90d62e1384cd961567ae123a494decbc44ba60e8
Jacob Hill <jacobhill.mail@gmail.com>
2019-03-01 15:45:25 -0500

    use oai and extract_oai instead of bnf and extract_xml

--- sakip_sabanci_config.rb
+++ sakip_sabanci_config.rb
@@ -4,41 +4,36 @@ require 'traject_plus'
 require 'dlme_json_resource_writer'
 require 'macros/dlme'
 require 'macros/oai'
-require 'macros/ssm'
 
 extend Macros::DLME
 extend Macros::OAI
 extend TrajectPlus::Macros
 extend TrajectPlus::Macros::Xml
 
-NS = { xmlns: 'http://www.openarchives.org/OAI/2.0/' }.merge(Macros::OAI::NS).freeze
-
 settings do
   provide 'writer_class_name', 'DlmeJsonResourceWriter'
   provide 'reader_class_name', 'TrajectPlus::XmlReader'
 end
 
-record = '/xmlns:record/xmlns:metadata/oai_dc:dc'
-
 # Cho Required
-to_field 'id', extract: extract_oai('dc:identifier'), transform: transform(strip: true)
-to_field 'cho_title', extract: extract_xml("#{record}/dc:title", NS), transform: transform(strip: true)
+to_field 'id', extract_oai('/dc:identifier'), strip
+to_field 'cho_title', extract_oai('/dc:title'), strip
 
 # Cho Other
-to_field 'cho_contributor', extract_xml("#{record}/dc:contributor", NS), transform: transform(split: split('.'), strip: true)
-to_field 'cho_coverage', extract_xml("#{record}/dc:coverage", NS), transform: transform(strip: true)
-to_field 'cho_creator', extract_xml("#{record}/dc:creator", NS), transform: transform(split: split('.'), strip: true)
-to_field 'cho_date', extract_xml("#{record}/dc:date", NS), transform: transform(strip: true)
-to_field 'cho_description', extract_xml("#{record}/dc:description", NS), transform: transform(strip: true)
-to_field 'cho_dc_rights', extract_xml("#{record}/dc:rights", NS), transform: transform(strip: true)
-to_field 'cho_edm_type', extract_xml("#{record}/dc:type", NS),
-         transform(strip: true, downcase: true), translation_map('not_found', 'types')
-to_field 'cho_format', extract_xml("#{record}/dc:format", NS), transform: transform(strip: true)
-to_field 'cho_language', extract_xml("#{record}/dc:language", NS), split(';'),
-         transform(strip: true, downcase: true), translation_map('not_found', 'languages', 'marc_languages')
-to_field 'cho_publisher', extract_xml("#{record}/dc:publisher", NS), transform: transform(strip: true)
-to_field 'cho_relation', extract_xml("#{record}/dc:relation", NS), transform: transform(strip: true)
-to_field 'cho_subject', extract_xml("#{record}/dc:subject", NS), transform: transform(strip: true)
+to_field 'cho_contributor', extract_oai('/dc:contributor'), transform(split: split('.'), strip: true)
+to_field 'cho_coverage', extract_oai('/dc:coverage'), strip
+to_field 'cho_creator', extract_oai('/dc:creator'), transform(split: split('.'), strip: true)
+to_field 'cho_date', extract_oai('/dc:date'), strip
+to_field 'cho_description', extract_oai('/dc:description'), strip
+to_field 'cho_dc_rights', extract_oai('/dc:rights'), strip
+to_field 'cho_edm_type', extract_oai('/dc:type'),
+         strip, translation_map('not_found', 'types')
+to_field 'cho_format', extract_oai('/dc:format'), strip
+to_field 'cho_language', extract_oai('/dc:language'), split(';'),
+         strip, translation_map('not_found', 'languages', 'marc_languages')
+to_field 'cho_publisher', extract_oai('/dc:publisher'), strip
+to_field 'cho_relation', extract_oai('/dc:relation'), strip
+to_field 'cho_subject', extract_oai('/dc:subject'), strip
 
 # Agg
 to_field 'agg_provider', provider
