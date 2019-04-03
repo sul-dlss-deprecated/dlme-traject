2c7887cad77a25be17b495ed5b7e41cd7e99e4c2
Jacob Hill <jacobhill.mail@gmail.com>
2019-04-03 10:57:10 -0700

    remove mods_config.rb from previous commit

--- bnf_ifao_config.rb
+++ bnf_ifao_config.rb
@@ -5,9 +5,5 @@ to_field 'cho_creator', extract_srw('dc:creator'), split('.'), first_only, strip
 to_field 'cho_creator', extract_srw('dc:creator[2]'), split('.'), first_only, strip
 to_field 'cho_creator', extract_srw('dc:creator[3]'), split('.'), first_only, strip
 to_field 'cho_edm_type', extract_srw('dc:type'),
-         default('notated music'),
-         first_only,
-         strip,
-         translation_map('types'),
-         default('image')
+         default('notated music'), first_only, strip, transform(&:downcase), translation_map('not_found', 'types', 'french-types')
 to_field 'cho_language', extract_srw('dc:language'), first_only, strip, translation_map('marc_languages')
