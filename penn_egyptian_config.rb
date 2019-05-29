955aa0cbf558bd2d46aeb04e4ee2b3dbe95a4064
Jacob Hill <jacobhill.mail@gmail.com>
2019-05-29 16:25:06 -0400

    Fix typo

--- penn_egyptian_config.rb
+++ penn_egyptian_config.rb
@@ -25,7 +25,7 @@ to_field 'cho_title', column('object_name'), split('|')
 # CHO Other
 to_field 'cho_coverage', column('culture'), split('|')
 to_field 'cho_creator', column('creator')
-to_field 'agg_data_provider', column('curatorial_section'), append('Section, Penn Museum')
+to_field 'agg_data_provider', column('curatorial_section'), append(' Section, Penn Museum')
 to_field 'cho_date', column('date_made')
 to_field 'cho_date', column('date_made_early')
 to_field 'cho_date', column('date_made_late')
