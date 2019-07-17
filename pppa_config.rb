a68551fff99f07d6c4aaae9c9f16bacdb5d82ce5
Jacob Hill <jacobhill.mail@gmail.com>
2019-07-17 15:21:42 -0700

    Add rights statement to pppa mapping

--- pppa_config.rb
+++ pppa_config.rb
@@ -24,6 +24,7 @@ to_field 'cho_date', column('Date')
 to_field 'cho_description', column('Description')
 to_field 'cho_edm_type', literal('Image')
 to_field 'cho_identifier', column('Resource-URL')
+to_field 'cho_rights', literal("The PPPA operates according to the principles of 'fair use'. According to fair use principles, an author may make limited use of another author's work without asking for permission. Fair use is based on the belief that the public is entitled to freely use portions of copyrighted materials for non-commercial educational purposes, commentary and criticism. For full rights policy, see: https://www.palestineposterproject.org/content/faq")
 to_field 'cho_subject', column('Subject')
 
 # Agg
