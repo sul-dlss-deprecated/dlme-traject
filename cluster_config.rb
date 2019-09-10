fabcb557be5401c1f4b8c776ca4935b4c439bed2
Jacob Hill <jacobhill.mail@gmail.com>
2019-09-10 13:16:55 -0400

    Test cluster mapping

--- cluster_config.rb
+++ cluster_config.rb
@@ -20,7 +20,6 @@ to_field 'cho_title', extract_json('.title'), strip
 to_field 'cho_creator', extract_json('.creator'), strip
 to_field 'cho_contributor', extract_json('.contributor'), strip
 to_field 'cho_date', extract_json('.date'), strip
-# to_field 'cho_dc_rights', literal('Photo: © Bodleian Libraries, University of Oxford, Terms of use: http://digital.bodleian.ox.ac.uk/terms.html')
 to_field 'cho_description', extract_json('.description'), strip
 to_field 'cho_edm_type', literal('Dataset')
 to_field 'cho_language', literal('English')
